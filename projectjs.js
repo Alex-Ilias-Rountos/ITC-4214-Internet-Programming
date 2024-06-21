289604d4a4d9800735bb3f1b7a7e21b675ad20a2
Alex-Ilias-Rountos <97444428+Alex-Ilias-Rountos@users.noreply.github.com>
2024-06-21 12:21:37 +0300

    implemented dark mode, first carousel attempt, initial JS

--- projectjs.js
+++ projectjs.js
@@ -7,15 +7,7 @@ function foo(){
 
 
 
-
-
-
-//make it so buttons dont call functions if not all form fields are filled.
-
-
-
-
-
+//make it so buttons dont call functions if not all cells are filled.
 
 
 
@@ -36,6 +28,16 @@ function ContactPopup2(){
 }
 
 
-
-
 //window.alert() for the Contact Us page popup?
+
+
+
+
+//Dark Mode?
+$(document).ready(function() {
+  $('#DMbutton').click(function() {
+      $('body').toggleClass('dark-mode');
+      $('header, main, footer, nav a').toggleClass('dark-mode');
+      //alert("dark mode button pressed");    //test line
+  });
+});
