2b96f099f42484694445b8d4f1c95dd97a3203e0
Alex-Ilias-Rountos <97444428+Alex-Ilias-Rountos@users.noreply.github.com>
2024-06-21 12:18:59 +0300

    Initial skeleton, index/home page, header and footer

---
+++ projectjs.js
@@ -0,0 +1,23 @@
+function foo(){
+   
+ window.alert("Button Pressed");
+
+}
+
+
+
+
+//Contact Us page JS
+
+function ContactPopup(){
+   
+    window.alert("Message Sent!");
+   
+   }
+
+
+
+
+
+
+//window.alert() for the Contact Us page popup?
